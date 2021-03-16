963f5dc225b6d216bb532bac1e6d1f7a30ac19dd
Giuseppe Lana <giuseppe.lana27@outlook.it>
2021-03-16 15:31:39 +0100

    inserita timing function con cui chiedo all'utente di inserire i numeri che ricorda

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto pr
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
 
 
-// 1. inizializzo un array che conterrà i numeri casuali
+// 1. inizializzo due array che conterranno i numeri casuali e quelli inseriti dall'utente
 var randomNumbers = [];
+var userNumbers = [];
 // inserisco i numeri random nell'array
 while (randomNumbers.length < 5){
   var number = randomNumbersGenerator();
@@ -16,6 +17,30 @@ while (randomNumbers.length < 5){
   }
 }
 console.log(randomNumbers);
+
+// 2. mostro alert con i numeri da ricordare
+alert("Questi sono i numeri da ricordare: " + randomNumbers);
+
+// 3. dopo 30 secondi chiedo all'utente di inserire i numeri che aveva visualizzato
+setTimeout(function () {
+  for ( var i = 0; i < 5; i++){
+    var userNumber = parseInt(prompt("Inserisci il numero che ricordi"));
+    if ( isNaN(userNumber)){
+      userNumber = parseInt(prompt("Inserisci il numero che ricordi in valore numerico"));
+      userNumbers.push(userNumber);
+    } else {
+      userNumbers.push(userNumber);
+    }
+  }
+  console.log(userNumbers);
+}, 5000);
+
+
+
+
+
+
+
 
 // funzione per generare numeri random
 function randomNumbersGenerator(){
